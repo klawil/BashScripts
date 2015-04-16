f843315a541040f0fd8bd438a39f129e0e2ec53b
klawil <wklausmeyer@yahoo.com>
2015-04-15 20:49:59 -0500

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install openssh-server apache2 audacity vlc python perl perl-modules php5 php5-common php5-curl php5-json php5-mysql remmina virtualbox wireshark deluge android-tools-adb screen default-jre default-jdk unrar gimp i3 git traceroute nmap npm curl nautilus-dropbox aircrack-ng mysql-client-5.5 mysql-server unity-tweak-tool node xclip dconf-editor gparted
+sudo apt-get purge transmission-gtk
+sudo apt-get update
+sudo apt-get upgrade
