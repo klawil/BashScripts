604f360b3a7769ac599a267b7dc76efb2f3ee911
klawil <wklausmeyer@yahoo.com>
2015-04-23 08:54:34 -0500

    Changed syntax to make less output

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@ red='\E[31m'
 blue='\E[34m'
 normal='\E[m'
 localip='192.168.0.12'
-basecommand="rsync -avzR $(cat ~/.include.rsync)"
-includefiles=$(cat ~/.include.rsync)
+basecommand="rsync --log-file=/home/william/backup.log --progress -aizR $(cat ~/.include.rsync)"
+echo "$basecommand"
 echo "Select a server:"
 echo -e "  ${green}[1]${normal} Backup to local server"
 echo -e "  ${green}[2]${normal} Backup to remote server"
@@ -15,10 +15,10 @@ echo -en "Select Option: "
 read optionnum
 case $optionnum in
 1)
-	$basecommand master@${localip}:/media/Files/laptop
+	$basecommand master@${localip}:/media/Files/laptop | grep --line-buffered '<\|>'
 	;;
 2)
-	$basecommand master@home.opfoolbird.com:1222/media/Files/laptop
+	$basecommand master@home.opfoolbird.com:1222/media/Files/laptop | grep --line-buffered '<\|>'
 	;;
 3)
 	rsync -avzR master@${localip}:/media/Files/laptop /
